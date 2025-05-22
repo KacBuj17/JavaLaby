02d6499ff6568cfac9d33b116e6dd60c14701b1b
KacBuj17 <kbujak150@interia.pl>
2025-05-22 19:50:16 +0200

    Update

--- BankingSystem/src/main/java/org/example/gui/GUI.java
+++ BankingSystem/src/main/java/org/example/gui/GUI.java
@@ -1,12 +1,19 @@
 package org.example.gui;
 
 import javax.swing.*;
+import com.formdev.flatlaf.*;
 
 public class GUI extends JFrame {
     public static String currentUserLogin;
     public static String currentAdminLogin;
 
     public static void run() {
-        SwingUtilities.invokeLater(() -> new WelcomeScreen().setVisible(true));
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(WelcomeScreen::new);
     }
 }
